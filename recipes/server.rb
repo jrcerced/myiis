916dc625d3f0ab6f01c4d7e1d6e62f61d00d035e
jr <jrcc@example.com>
2019-06-18 19:36:07 +0000

    Version 0.2.0 con info de maq.

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,7 +4,11 @@ end
 
 
 file 'C:\inetpub\wwwroot\Default.htm' do
-  content '<h1> Hola, gentes del mundo</h1>'
+  content "<h1> Hola, gentes del mundo</h1>
+  <h2>PLATFORM: #{node['platform']}</h2>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>MEMORY: #{node['memory']['total']}</h2>
+  <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2> "
 end
 
 service 'w3svc' do
